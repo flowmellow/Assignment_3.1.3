ab6e78cd8faee7a448570f8ac753eaefddee891c
flowmellow <michael.floresmmt@gmail.com>
2024-05-21 09:14:31 -0400

    Add project files.

---
+++ Assignment_3.1.3/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Write a program in C# Sharp to create a function to input a string and count number of spaces are in the string.
+Test Data :
+Please input a string : This is a test string.
+Expected Output :
+"This is a test string." contains 4 spaces
+*/
+
+using System.Threading.Channels;
+
+namespace Assignment_3._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a string: ");
+            string input = Console.ReadLine(); 
+            char targetChar = ' ';
+            int charCount = input.Count(c => c == targetChar);
+            Console.WriteLine($"\"{input}\" contains {charCount} spaces.");
+        }
+
+
+        
+        
+
+    }
+}
